cbe169c92462ff344e7e009de1669335e5c01866
Felipe Santos <fellypedamiao@hotmail.com>
2025-05-17 23:12:03 +0100

    Add files via upload

---
+++ lowest_spender_client.sql
@@ -0,0 +1,12 @@
+-- Client who spent the least
+
+SELECT
+  c.customer_id AS client_id,
+  c.name AS client_name,
+  CONCAT('€', FORMAT('%0.2f', SUM(o.amount))) AS spent_client
+FROM `polynomial-coda-457620-c7.ordersanalysis.orders` o
+JOIN `polynomial-coda-457620-c7.ordersanalysis.customers` c 
+  ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.name
+ORDER BY SUM(o.amount) ASC
+LIMIT 1;
