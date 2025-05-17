4bf9e82dd80cbb1f04b10c40e4e49a4c480e0c10
Felipe Santos <fellypedamiao@hotmail.com>
2025-05-17 23:01:23 +0100

    Add files via upload

--- queries/orders_by_client.sql
+++ queries/orders_by_client.sql
@@ -1,11 +1,11 @@
--- Clients who spent the most in the store
+-- Number of orders per client
 
 SELECT
   c.customer_id AS client_id,
   c.name AS client_name,
-  CONCAT('€', FORMAT('%0.2f', SUM(o.amount))) AS total_spent
+  COUNT(o.order_id) AS client_orders
 FROM `polynomial-coda-457620-c7.ordersanalysis.orders` o
 JOIN `polynomial-coda-457620-c7.ordersanalysis.customers` c 
   ON c.customer_id = o.customer_id
 GROUP BY c.customer_id, c.name
-ORDER BY SUM(o.amount) DESC;
+ORDER BY client_orders ASC;
--- queries/top_spender_client.sql
+++ queries/top_spender_client.sql
@@ -1,11 +1,12 @@
--- Clients who spent the most in the store
+-- Top client by total spending
 
 SELECT
   c.customer_id AS client_id,
   c.name AS client_name,
-  CONCAT('€', FORMAT('%0.2f', SUM(o.amount))) AS total_spent
+  CONCAT('€', FORMAT('%0.2f', SUM(o.amount))) AS spent_client
 FROM `polynomial-coda-457620-c7.ordersanalysis.orders` o
 JOIN `polynomial-coda-457620-c7.ordersanalysis.customers` c 
   ON c.customer_id = o.customer_id
 GROUP BY c.customer_id, c.name
-ORDER BY SUM(o.amount) DESC;
+ORDER BY SUM(o.amount) DESC
+LIMIT 1;
